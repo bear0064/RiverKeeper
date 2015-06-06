e78e124ba794444831ca3dbd345b729fce4101d8
Rob Young <rob.young@canarie.ca>
2015-06-06 19:30:37 -0400

    Cleaned up form JS a little.

--- js/geocoding.js
+++ js/geocoding.js
@@ -5,29 +5,25 @@ function lookupPostal() {
 
 	$.ajax({url:"http://api.zippopotam.us/ca/" + postalCode}).then(function(place) {
 		if (place == {}) {
-			alert("Could not find your postal code!");
+			alert("Could not look up the postal code " + postalCode);
 		}
 		else if (place.places.length > 0) {
 			var location = place.places[0];
 			var lat = location.latitude;
 			var long = location.longitude;
 			map.setView([lat, long], 9);
-			console.log(lat + ", " + long);
+
 			if (marker == null) {
 				marker = L.marker([lat, long]).addTo(map);
-				/*marker.bindPopup("Please add your activities here!<br />[ " + lat + ", " + long + ' ]<br />' + location['place name']).openPopup();
-				*/
-				marker.bindPopup($("#map-form").html()).openPopup();
-				// set lat long here!
 			}
 			else {
 				marker.setLatLng([lat, long]);
-				marker.bindPopup($("#map-form").html()).openPopup();
-				// set lat long here!
 			}
+
+			marker.bindPopup($("#map-form").html()).openPopup();
 		}
 		else {
-			console.log('not found!');
+			alert("Could not look up the postal code " + postalCode);
 		}
     }, function() {
 		alert("Could not look up the postal code " + postalCode);
